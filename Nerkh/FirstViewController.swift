0c8a3e738db19a13d31f28d46ea3b33bcb561757
Sina Khanjani <Sinakhanjani@gmail.com>
2020-03-05 11:44:41 +0330

    Initial Commit

---
+++ Nerkh/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Nerkh
+//
+//  Created by Sina khanjani on 12/15/1398 AP.
+//  Copyright © 1398 Sina Khanjani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NerkhTests/NerkhTests.swift
@@ -0,0 +1,34 @@
+//
+//  NerkhTests.swift
+//  NerkhTests
+//
+//  Created by Sina khanjani on 12/15/1398 AP.
+//  Copyright © 1398 Sina Khanjani. All rights reserved.
+//
+
+import XCTest
+@testable import Nerkh
+
+class NerkhTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
