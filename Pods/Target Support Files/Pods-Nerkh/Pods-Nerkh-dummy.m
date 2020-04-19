b9e973bb5539839ba1ae5782854224dab78f6632
Sina Khanjani <Sinakhanjani@gmail.com>
2020-04-19 15:34:12 +0430

    ..

---
+++ Pods/Target Support Files/Pods-Nerkh/Pods-Nerkh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Nerkh : NSObject
+@end
+@implementation PodsDummy_Pods_Nerkh
+@end
---
+++ Pods/Target Support Files/Pods-NerkhTests/Pods-NerkhTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NerkhTests : NSObject
+@end
+@implementation PodsDummy_Pods_NerkhTests
+@end
